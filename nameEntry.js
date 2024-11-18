0e9422b69cc88e7abd6d5dc1d59f27c41150a5ef
safepage101 <louisarmanaceron2@gmail.com>
2024-11-18 12:33:23 +0800

    Update nameEntry.js

--- nameEntry.js
+++ nameEntry.js
@@ -1,12 +1,12 @@
 document.getElementById('nameForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-    
+    event.preventDefault(); 
     const submitButton = document.querySelector('button[type="submit"]');
     const loader = document.getElementById('loader');
     const submitText = document.querySelector('.submit-text');
     
     const nameInput = document.getElementById('customerName').value.trim();
     const lastnameInput = document.getElementById('customerLastname').value.trim();
+
     
     loader.style.display = 'inline-block';
     submitText.style.display = 'none';
@@ -23,10 +23,9 @@ document.getElementById('nameForm').addEventListener('submit', function (event) 
 
     const capitalizedName = nameInput.charAt(0).toUpperCase() + nameInput.slice(1).toLowerCase();
     const capitalizedLastname = lastnameInput.charAt(0).toUpperCase() + lastnameInput.slice(1).toLowerCase();
-    const currentDateTime = new Date().toISOString();
 
-    fetch(`https://script.google.com/macros/s/AKfycbwVrMjQS5FpSIzix2mG-s4oH0LGdr4QghSuoR-4Ci0dUs-z-m05P0bCHAAAwN-jd7E3/exec?name=${encodeURIComponent(capitalizedName)}&lastname=${encodeURIComponent(capitalizedLastname)}&datetime=${encodeURIComponent(currentDateTime)}`)
-        .then(response => response.text())
+    fetch(`https://script.google.com/macros/s/AKfycbzuTyCL5bfJ_WfECKqFcSTJOpChuXEtv1ti4Hyd60usFrKuUir9zj4GiTGgJPTYgkalUA/exec?name=${encodeURIComponent(capitalizedName)}&lastname=${encodeURIComponent(capitalizedLastname)}`)
+        .then(response => response.text()) 
         .then(data => {
             console.log(data); 
             const urlParams = new URLSearchParams(window.location.search);
